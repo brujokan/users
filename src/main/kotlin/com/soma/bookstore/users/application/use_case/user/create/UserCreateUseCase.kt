9bc4fa68b48dc254d03ab4044ee20e443cc50c59
luis.manzanaro@gmail.com <luis.manzanaro@gmail.com>
2022-12-12 18:54:47 +0100

    Latest changes

--- src/main/kotlin/com/soma/bookstore/users/application/use_case/user/create/UserCreateUseCase.kt
+++ src/main/kotlin/com/soma/bookstore/users/application/use_case/user/create/UserCreateUseCase.kt
@@ -3,6 +3,7 @@ package com.soma.bookstore.users.application.use_case.user.create
 import com.soma.bookstore.users.domain.messaging.UserProducer
 import com.soma.bookstore.users.domain.model.User
 import com.soma.bookstore.users.domain.repository.UserRepository
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
@@ -12,10 +13,15 @@ class UserCreateUseCase(
     private val userCreateProducer: UserProducer
     ) {
 
+    private val logger = KotlinLogging.logger {  }
+
     fun save(user: User): User {
         val userSaved = repository.save(user)
-        userCreateProducer.send(userSaved)
-
+        try {
+            userCreateProducer.send(userSaved)
+        } catch (ex: Exception) {
+            logger.error { "Error: ${ex.message}" }
+        }
         return userSaved
     }
 }
